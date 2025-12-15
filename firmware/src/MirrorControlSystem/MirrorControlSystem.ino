ab11ce66499e98c9e818ce1dda56c5be525b31cb
Toni Matutinović <tonimatutta@gmail.com>
2025-12-15 23:36:18 +0100

    feat(firmware): add smooth movement to central position on switch press

--- firmware/src/MirrorControlSystem/MirrorControlSystem.ino
+++ firmware/src/MirrorControlSystem/MirrorControlSystem.ino
@@ -18,14 +18,17 @@ int xStaticMin = 510;
 int xStaticMax = 540;
 int yStaticMin = 520;
 int yStaticMax = 550;
+int lastSwitch = 1;
 
 // y - axis servo
+float centralPosY = 40;
 float posY = 40;
 const float minPosY = 27.5;
 const float maxPosY = 52.5;
 const float deltaY = 0.25;
 
 // x - axis servo
+float centralPosX = 20;
 float posX = 20;
 const float minPosX = 0;
 const float maxPosX = 40;
@@ -37,6 +40,9 @@ const float delta = 0.5;
 // delay
 int dt = 30;
 
+// central movement
+bool movingToCenter = false;
+
 void getJoystickValue() {
     xVal = analogRead(X_PIN);
     yVal = analogRead(Y_PIN);
@@ -44,25 +50,52 @@ void getJoystickValue() {
 }
 
 void updateServo() {
-    // X-axis movement
-    if (xVal < xStaticMin) {
-        posX -= deltaX;
-    } else if (xVal > xStaticMax) {
-        posX += deltaX;
+    if (!movingToCenter){
+        // X-axis movement
+        if (xVal < xStaticMin) {
+            posX -= deltaX;
+        } else if (xVal > xStaticMax) {
+            posX += deltaX;
+        }
+
+        if (posX < minPosX) posX = minPosX;
+        if (posX > maxPosX) posX = maxPosX;
+
+        // Y-axis movement
+        if (yVal < yStaticMin) {
+            posY += deltaY;
+        } else if (yVal > yStaticMax) {
+            posY -= deltaY;
+        }
+
+        if (posY < minPosY) posY = minPosY;
+        if (posY > maxPosY) posY = maxPosY;
+    }
+}
+
+void moveToCentralPosition() {
+    if (!movingToCenter) return;
+
+    // X-axis
+    if (abs(posX - centralPosX) > delta) {
+        if (posX < centralPosX) posX += delta;
+        else posX -= delta;
+    } else {
+        posX = centralPosX;
     }
 
-    if (posX < minPosX) posX = minPosX;
-    if (posX > maxPosX) posX = maxPosX;
-
-    // Y-axis movement
-    if (yVal < yStaticMin) {
-        posY += deltaY;
-    } else if (yVal > yStaticMax) {
-        posY -= deltaY;
+    // Y-axis
+    if (abs(posY - centralPosY) > delta) {
+        if (posY < centralPosY) posY += delta;
+        else posY -= delta;
+    } else {
+        posY = centralPosY;
     }
 
-    if (posY < minPosY) posY = minPosY;
-    if (posY > maxPosY) posY = maxPosY;
+    // Check if reached center
+    if (posX == centralPosX && posY == centralPosY) {
+        movingToCenter = false;
+    }
 }
 
 void setup() {
@@ -78,7 +111,15 @@ void setup() {
 
 void loop() {
     getJoystickValue();
+
+    // Check for switch press
+    if (lastSwitch == 0 && switchVal == 1) {
+        movingToCenter = true;
+    }
+    lastSwitch = switchVal;
+
     updateServo();
+    moveToCentralPosition();
 
     xServo.write(posX);
     yServo.write(posY);
